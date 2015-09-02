b835e939946bd183ebbcef01c850abf668f670f9
velozmontero <velozmontero@gmail.com>
2015-09-01 20:10:04 -0400

    run multiple functions

---
+++ runmultiple_function.js
@@ -0,0 +1,15 @@
+function f1(){
+    
+}
+
+function f2(){
+    
+}
+
+document.getElementById('#').onchange = function = function() {
+    f1();
+    f2();
+};
+
+document.getElementById('#').addEventListener("change", f1);
+document.getElementById('#').addEventListener("change", f2);
